0bb91a9b54895cdf7534847eab9cd7958d701ada
DocGashe <gasheva_99@mail.ru>
2023-01-14 11:02:54 +0500

    Add default locale

--- src/filters/birth.ts
+++ src/filters/birth.ts
@@ -1,6 +1,6 @@
 export enum Locale {RU = 'ru-Ru'}
 
-export function dateToLocale(value: Date, locale: Locale): string {
+export function dateToLocale(value: Date, locale: Locale = Locale.RU): string {
     switch (locale) {
         case Locale.RU:
             return new Intl.DateTimeFormat(locale).format(new Date(value));
